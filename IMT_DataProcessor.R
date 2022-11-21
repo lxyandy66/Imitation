537b5bf1d806b7f5c89d2b1ac49d5a342a3eab41
Li Xinyue <lxy_2008focus@126.com>
2022-11-21 21:05:18 +0800

    更新了对模拟结果的统计分析脚本(R);

--- IMT_DataProcessor.R
+++ IMT_DataProcessor.R
@@ -23,10 +23,25 @@ data.imt.net.raw<-fread(file="1020_Pre_NetworkExchangeImport.csv")
 # 按照MsgIdRcv（time-trigger）统计整体，统计所得丢包率不对
 # 一个发送间隔内，接收端可能收到多个包，因此统计的丢包率不对，且偏大
 
+# 同一个包可能被多个采样次数记录，故选取最新的一个，即时间最早的一个
 data.imt.net.rcv<-data.imt.net.raw[,.(rcvTime=min(time,na.rm = TRUE)),by=MsgIdRcv]
 data.imt.net.snd<-data.imt.net.raw[,.(sndTime=min(time,na.rm = TRUE)),by=MsgIdSnd]
 
+# 合并原则，发送的不一定被接收到
+# msgIdSnd=sndTime
 data.imt.net.exchange<-merge(x=data.imt.net.snd,y=data.imt.net.rcv,
                              all.x = TRUE,by.x="MsgIdSnd",by.y="MsgIdRcv")
-data.imt.net.exchange$isNewest<-apply()
 
+# 根据时间判断是否active packet loss，即收到但为旧的包
+# 验证case: ID=112 应为主动丢包
+data.imt.net.exchange$isNewest<-apply(X = data.imt.net.exchange[,c("MsgIdSnd","rcvTime")],MARGIN = 1,
+                                      FUN = function(x){
+                                        return(x[2]>=max(data.imt.net.exchange[MsgIdSnd<x[1]]$rcvTime,na.rm = TRUE))
+                                      })
+
+# 1020_Pre 模拟结果记录
+# 主动丢包   正常    丢包
+# FALSE  TRUE NA
+# 26   756   19
+# 总丢包率 0.056
+# 实际丢包率 0.024
